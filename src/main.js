ec26d095cf1c4750813c6cb41f389c35d5d6c39d
Kent <demonwhite.tw@gmail.com>
2020-09-02 18:50:41 -0400

    custom style working!!

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import DefaultLayout from '~/layouts/Default.vue'
 import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import '~/assets/styles.scss'
+// import 'buefy/dist/buefy.css'
 export default function (Vue, { router, head, isClient }) {
+  // add font Montserrat
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
+  })
   // adding material design icons
   head.link.push({
     rel: 'stylesheet',
